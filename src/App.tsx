52ab785ec732a39e194f6eb6bdcde41096a3f6ce
Rcong <zcr0903@gmail.com>
2022-08-01 20:06:18 +0800

    提交一些前端代码

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,13 @@
-import { defineComponent, ref } from "vue";
+import { defineComponent } from "vue";
+import { RouterView } from "vue-router";
+import "./App.scss"
 
 export const App = defineComponent({
   setup() {
-    const refCount = ref(0);
-    const onClick = () => {
-      refCount.value += 1;
-    };
-
-    return () => <>
-      <div>{refCount.value}</div>
+    return () => (
       <div>
-        <button onClick={onClick}></button>
+        <RouterView />
       </div>
-    </>
-  },
-});
+    )
+  }
+})
