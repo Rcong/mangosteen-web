6aabb8cc24f3501adb378ff074343afe094f008c
Rcong <zcr0903@gmail.com>
2022-08-07 15:16:56 +0800

    useSwipe

--- src/views/Welcome.tsx
+++ src/views/Welcome.tsx
@@ -1,10 +1,16 @@
 import { defineComponent, ref, Transition, VNode, watchEffect } from 'vue';
-import { RouteLocationNormalizedLoaded, RouterView } from 'vue-router';
+import { RouterView } from 'vue-router';
+import { useSwipe } from '../hooks/useSwipe';
 import s from './Welcome.module.scss'
-import logo from 'src/assets/icons/mangosteen.svg'
 
 export const Welcome = defineComponent({
   setup: (props, context) => {
+    const main = ref<HTMLElement | null>(null)
+    const { direction, swiping } = useSwipe(main)
+    watchEffect(() => {
+      console.log(swiping.value, direction.value)
+    })
+
     return () => <div class={s.wrapper}>
       <header>
         <svg>
@@ -12,7 +18,7 @@ export const Welcome = defineComponent({
         </svg>
         <h1>山竹记账</h1>
       </header>
-      <main class={s.main}>
+      <main class={s.main} ref={main}>
         <RouterView name="main">
           {
             ({ Component: component }: { Component: VNode }) =>
